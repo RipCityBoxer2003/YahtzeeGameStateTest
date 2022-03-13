64e106d58a1df205bc3beea395b02bb28fdc8d77
Augustine Pham <pham24@up.edu>
2022-03-13 15:08:17 -0700

    Add files via upload
    dice and overall game state class

---
+++ app/src/main/java/com/example/yahtzeegamestatetest/Dice.java
@@ -0,0 +1,46 @@
+package edu.up.cs301.pig;
+
+public class Dice {
+    private int val;
+    private boolean keep;
+
+    /**
+     * Dice ctor
+     */
+    public Dice() {
+        this.val = 0;
+        this.keep = false;
+    }
+
+    //TODO getters and setters
+
+    //copy ctor
+    public Dice(Dice dice) {
+        this.val = dice.val;
+        this.keep = dice.keep;
+    }
+
+    //getter methods
+    public int getVal(){
+        return val;
+    }
+
+    //If true, its a keeper
+    public boolean isKeep() {
+        return keep;
+    }
+
+    //setter methods
+    public void setKeep(boolean keep) {
+        this.keep = keep;
+    }
+
+    @Override
+    public String toString(){
+        return "Dice Value: " + val + " Dice Keeper: " + keep;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+}
